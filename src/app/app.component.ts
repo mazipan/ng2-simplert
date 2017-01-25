7be701a2b8ecb26897040f5963765ec6a6918278
Irfan Maulana <irfan.maulana@gdn-commerce.com>
2017-01-25 16:56:48 +0700

    update demo template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,9 +4,14 @@ import { SimplertService } from './simplert.component.service';
 @Component({
   selector: 'app-root',
   template: `
-    <button class="open--info" (click)="showPopup('Information!', 'Hey, I am Opened...')">Open Information Alert</button>
-    <button class="open--error" (click)="showPopup('Error!', 'Hey, I am Opened...')">Open Error Alert</button>
-    <button class="open--warning" (click)="showPopup('Warning!', 'Hey, I am Opened...')">Open Warning Alert</button>
+    <h1>Ng2-Simplert</h1>
+    <h5>By Irfan Maulana</h5>
+    <div>
+      <button class="open--info" (click)="showPopup('Information!', 'Hey, I am Opened...')">Open Information Alert</button>
+      <button class="open--error" (click)="showPopup('Error!', 'Hey, I am Opened...')">Open Error Alert</button>
+      <button class="open--warning" (click)="showPopup('Warning!', 'Hey, I am Opened...')">Open Warning Alert</button>    
+    </div>
+    <a href="https://github.com/mazipan/ng2-simplert" target="_blank">Fork on Github</a>
     <SimplertComponent [isUseRadius]="true"></SimplertComponent>
   `
 })
