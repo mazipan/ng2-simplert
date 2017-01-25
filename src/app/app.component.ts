3f586799bd8d027dfdf66ca2fb9d475024070c63
Irfan Maulana <irfan.maulana@gdn-commerce.com>
2017-01-25 16:53:04 +0700

    change attribute

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,7 @@ import { SimplertService } from './simplert.component.service';
     <button class="open--info" (click)="showPopup('Information!', 'Hey, I am Opened...')">Open Information Alert</button>
     <button class="open--error" (click)="showPopup('Error!', 'Hey, I am Opened...')">Open Error Alert</button>
     <button class="open--warning" (click)="showPopup('Warning!', 'Hey, I am Opened...')">Open Warning Alert</button>
-    <SimplertComponent isUseRadius="true"></SimplertComponent>
+    <SimplertComponent [isUseRadius]="true"></SimplertComponent>
   `
 })
 export class AppComponent {
